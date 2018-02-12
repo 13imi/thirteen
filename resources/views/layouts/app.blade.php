f71760e5565e99f373d3ddea19fa00d944382809
13imi <yuma.imi13@gmail.com>
2018-02-12 23:42:52 +0900

    Second Commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,19 +1,33 @@
 <!doctype html>
 <html @php(language_attributes())>
-  @include('partials.head')
+         @include('partials.head')
   <body @php(body_class())>
-    @php(do_action('get_header'))
-    @include('partials.header')
+         @php(do_action('get_header'))
+         @include('partials.header')
     <div class="wrap container" role="document">
       <div class="content">
-        <main class="main">
-          @yield('content')
-        </main>
-        @if (App\display_sidebar())
-          <aside class="sidebar">
-            @include('partials.sidebar')
-          </aside>
-        @endif
+        <div class="row">
+          @if(is_single())
+            <div class="breadcrumbs col-md-12" typeof="BreadcrumbList" vocab="https://schema.org/">
+              <i class="fas fa-folder-open"></i>
+              <?php if(function_exists('bcn_display'))
+              {
+              bcn_display();
+              }?>
+            </div>
+          @endif
+
+          <main class="main col-md-8">
+            <div class="col-card">
+              @yield('content')
+            </div>
+          </main>
+          @if (App\display_sidebar())
+            <aside class="sidebar col-md-4">
+              @include('partials.sidebar')
+            </aside>
+          @endif
+        </div>
       </div>
     </div>
     @php(do_action('get_footer'))
