7ef933a595ceb2a7e8c37ee0ccc10dafd7537256
13imi <yuma.imi13@gmail.com>
2018-02-13 14:18:18 +0900

    Add Site logo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,14 @@
 <header class="banner">
   <div class="container-fluid">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
+    <div class="logo">
+      <a class="brand" href="{{ home_url('/') }}">
+        <img src="@asset('images/icon_FBBLIFE.png')">
+                  </a>
+                  </div>
+                  <nav class="nav-primary">
+        @if (has_nav_menu('primary_navigation'))
+          {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+        @endif
     </nav>
-  </div>
+    </div>
 </header>
