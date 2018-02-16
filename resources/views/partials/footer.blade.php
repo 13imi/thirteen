ad124d6f5158570f94f59b3b94b9a5ad20170e70
13imi <yuma.imi13@gmail.com>
2018-02-17 01:45:28 +0900

    Fix Small

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="content-info">
   <div class="container">
     @php(dynamic_sidebar('sidebar-footer'))
-    <small><a href="/">Copyright (C) 2018 FBB LIFE All Rights Reserved.</a></small>
+    <small><a href="/">Copyright (C) 2018 Babybox life All Rights Reserved.</a></small>
   </div>
 </footer>
