62030bd5123022342efa4729103ea75e555d0e80
13imi <yuma.imi13@gmail.com>
2018-02-23 12:46:35 +0900

    Add gtm plugin in header.php

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,6 @@
+<!--[Google Tag Manager Plugin]>
+<?php if ( function_exists( 'gtm4wp_the_gtm_tag' ) ) { gtm4wp_the_gtm_tag(); } ?>
+
 <header class="banner">
   <div class="container-fluid">
     <div class="logo">
