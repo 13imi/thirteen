01b3e6e0c9a31ecd5b800f3bd65101016ed2ab20
13imi <yuma.imi13@gmail.com>
2018-03-18 13:22:07 +0900

    Fix Footer style

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,13 +1,13 @@
 <footer class="content-info">
   <div class="container">
     <div class="row">
-      <div class="col-md-4">
+      <div class="col-md-4 footer1">
         @php(dynamic_sidebar('sidebar-footer'))
       </div>
-      <div class="col-md-4">
+      <div class="col-md-4 footer2">
         @php(dynamic_sidebar('sidebar-footer2'))
       </div>
-      <div class="col-md-4">
+      <div class="col-md-4 footer3">
         @php(dynamic_sidebar('sidebar-footer3'))
       </div>
     </div>
