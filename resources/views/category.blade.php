aabe2523b48833b39d10737e6f5950dc6ca11163
13imi <yuma.imi13@gmail.com>
2018-05-03 01:40:43 +0900

    Fix category page design2

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,11 +1,11 @@
-@extends('layouts.top')
+@extends('layouts.app')
 
 @section('content')
   <div class="entry-list">
     <h3 class="section-title">「{!! App::title() !!}」の記事一覧</h3>
     <div class="row">
       @while(have_posts()) @php(the_post())
-        @include('partials.cards')
+        @include('partials.category-cards')
       @endwhile
     </div>
   </div>
---
+++ resources/views/partials/category-cards.blade.php
@@ -0,0 +1,13 @@
+<div class="col-md-6">
+  <a href="{{ get_permalink() }}">
+  <div class="card">
+    <img class="card-img-top" src="{{ the_post_thumbnail_url( 'large' ) }}" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">{{ get_the_title() }}</h5>
+      <span class="card-category"></span>
+      <p class="card-text"><time class="updated" datetime="{{ get_post_time('c', true) }}">{{ get_the_date() }}</time>
+      </p>
+    </div>
+  </div>
+  </a>
+</div>
