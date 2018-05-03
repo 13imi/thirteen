2f937d43595c5074da99f60bd987efca5b34e2f7
13imi <yuma.imi13@gmail.com>
2018-05-03 12:59:40 +0900

    Delete Google auto ads

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,13 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-  <script>
-   (adsbygoogle = window.adsbygoogle || []).push({
-       google_ad_client: "ca-pub-3789463449588080",
-       enable_page_level_ads: true
-   });
-  </script>
   <link rel="shortcut icon" href="@asset('images/icon-13.png')">
   @php(wp_head())
   <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
