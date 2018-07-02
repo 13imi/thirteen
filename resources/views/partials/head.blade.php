781de84de4dd58ea798f31d5e3b84db692a86b3c
13imi <yuma.imi13@gmail.com>
2018-07-03 08:32:12 +0900

    Add icon & Fix favicon

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,7 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="shortcut icon" href="@asset('images/icon-13.png')">
+  <link rel="shortcut icon" href="@asset('images/icon_imi.png')">
              @php(wp_head())
              <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
   @include('partials/head-custom-field')
